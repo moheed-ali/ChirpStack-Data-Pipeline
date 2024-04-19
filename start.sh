6b6c5eb1725b35b2003687ebfa38badfa8fe4a2e
Moheed Ali <moheedalikayani@outlook.com>
2024-04-19 11:04:00 +0200

    Create Start.sh file

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# To Make the File Executable
+# chmod +x start.sh
+
+# Run Python files
+
+# Promethus Data Collection
+python3 /home/eleve/ChirpStack-Data-Pipeline/Promethus/prom_to.py
+# echo "Python script executed successfully"
+# python3 file2.py &
+
+# Run C++ file
+# ./file3
