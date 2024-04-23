b5251ef56a91f4adc1888e904e22cfca1aa7d5eb
Moheed Ali <moheedalikayani@outlook.com>
2024-04-23 11:04:43 +0200

    Update include all files

--- start.sh
+++ start.sh
@@ -1,13 +1,44 @@
-#!/bin/bash
-# To Make the File Executable
-# chmod +x start.sh
+# #!/bin/bash
+# # To Make the File Executable
+# # chmod +x start.sh
 
+# # Run Python files
+
+# # Promethus Data Collection
+# python3 /home/eleve/ChirpStack-Data-Pipeline/main.py &
+
+# python3 /home/eleve/ChirpStack-Data-Pipeline/Promethus/prom_to.py &
+
+# python3 /home/eleve/ChirpStack-Data-Pipeline/Monitor/telegraf-data/collect.py
+# # echo "Python script executed successfully"
+# # python3 file2.py &
+
+# # Run C++ file
+# # ./file3
+
+
+
+#!/bin/bash
 # Run Python files
 
-# Promethus Data Collection
-python3 /home/eleve/ChirpStack-Data-Pipeline/Promethus/prom_to.py
-# echo "Python script executed successfully"
-# python3 file2.py &
+# Function to stop processes
+stop_processes() {
+    echo -e "\e[31mStopping Python scripts...\e[0m"  # Red color for stopping message
+    pkill -f "python3 /home/eleve/ChirpStack-Data-Pipeline/main.py"
+    pkill -f "python3 /home/eleve/ChirpStack-Data-Pipeline/Promethus/prom_to.py"
+    pkill -f "python3 /home/eleve/ChirpStack-Data-Pipeline/Monitor/telegraf-data/collect.py"
+    echo -e "\e[32mPython scripts stopped.\e[0m"  # Green color for stopped message
+}
 
-# Run C++ file
-# ./file3
+# Trap SIGINT (Ctrl+C) and SIGTERM signals to stop processes before exiting
+trap 'stop_processes; exit' SIGINT SIGTERM
+
+# Start Python scripts
+echo -e "\e[33mStarting Python scripts...\e[0m"  # Yellow color for starting message
+
+python3 /home/eleve/ChirpStack-Data-Pipeline/main.py &
+python3 /home/eleve/ChirpStack-Data-Pipeline/Promethus/prom_to.py &
+python3 /home/eleve/ChirpStack-Data-Pipeline/Monitor/telegraf-data/collect.py &
+
+# Wait for the processes to finish
+wait
